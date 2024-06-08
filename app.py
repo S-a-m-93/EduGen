f0295fb47989b136c9b228924d95851d9a2bf568
Sameera Kusala <124641896+S-a-m-93@users.noreply.github.com>
2024-06-08 11:32:25 +0530

    Added the necessary files

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+import google.generativeai as genai
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Define different prompts
+prompts = {
+    "Notes": "You are a YouTube video notes maker for students. You will be taking the transcript text and generate notes for the entire video. You will give all the important concepts given in the video in a concise and organized format without missing anything. The transcript text is:",
+    "Summary": "You are a YouTube video summary maker for students. You will be taking the transcript text and generate a summary for the entire video. You will provide a concise overview of the main points discussed in the video. The transcript text is:",
+    "Quiz": "You are a YouTube video quiz maker for students. You will be taking the transcript text and generate a quiz for the entire video. You will provide several questions and answers based on the key concepts discussed in the video. The transcript text is:",
+}
+
+
+## Getting the transcript from YouTube videos
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id = youtube_video_url.split("=")[1]
+        print(video_id)
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+        return transcript
+    except Exception as e:
+        raise e
+
+
+## Generating content based on the selected type
+def generate_gemini_content(transcript_text, content_type):
+    prompt = prompts[content_type]
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt + transcript_text)
+    return response.text
+
+
+st.title("📚 EduGen: Make the most of educational videos")
+
+youtube_link = st.text_input("Enter the YouTube Video Link")
+content_type = st.selectbox("Select content type", ["Notes", "Summary", "Quiz"])
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    st.image(f"https://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Generate Content"):
+    transcript_text = extract_transcript_details(youtube_link)
+    if transcript_text:
+        content = generate_gemini_content(
+            transcript_text=transcript_text, content_type=content_type
+        )
+        st.markdown(f"## {content_type}:")
+        st.write(content)
